6d9a55b0f95838c63f7f7e1f0410316e3b5ac89f
Herineu <189722065+Herineu@users.noreply.github.com>
2025-04-04 10:50:18 -0300

    Initial commit

---
+++ name_projet/src/main/java/neu/em/name_projet/NameProjetApplication.java
@@ -0,0 +1,13 @@
+package neu.em.name_projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameProjetApplication.class, args);
+	}
+
+}
---
+++ name_projet/src/test/java/neu/em/name_projet/NameProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package neu.em.name_projet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
